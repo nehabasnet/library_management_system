6405a5598fc87316c34a6e749129b0814ca5fd1d
Neha Basnet <nehabasnet2@gmail.com>
2026-05-24 14:56:19 +0545

    Login Page added

--- library_management/library/views.py
+++ library_management/library/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 def home(request):
     return render(request, 'library/index.html')
+
+
+def login_view(request):
+
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+        else:
+            return render(request, 'library/login.html', {
+                'error': 'Invalid username or password'
+            })
+
+    return render(request, 'library/login.html')
